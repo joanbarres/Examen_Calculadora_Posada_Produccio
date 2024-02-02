4780bd0e02c7dbbe085a2b87c8ad0c44d7741c42
Joan Barres <joanbarressab@gmail.com>
2024-02-02 18:47:00 +0100

    Creacion de fichero y programacion del menu

---
+++ menu.py
@@ -0,0 +1,20 @@
+
+def menu():
+    cadena = "Calaculadora by Joan Barres"
+    print(cadena.center(60,'_'))
+    print("La calculadora funciona de la siguiente manera:")
+    print("- 1º Introduzca la operacion a realizar")
+    print("  + ---> para realizar una suma")
+    print("  - ---> para realizar una resta")
+    print("  / ---> para realizar una division")
+    print("  * ---> para realizar una multiplicacion")
+    print("  % ---> para realizar una division entera")
+    print("  ! ---> para calcular el factorial de un numero")
+    print("  ? ---> para saber si el numero es primo o no")
+    print("  @ ---> para calcular la media de varios numeros")
+    print("- 3º A continuacion tiene varias opciones")
+    print("    -En caso de querer realizar una suma, resta, division, division entera o multiplicacion introduzca dos numeros")
+    print("    -En caso de querer realizar el factorial o calcular si es primo un numero")
+    print("    -En caso de querer calcular la media de varios numeros, introduzca numeros,cuando termine introducela palabra 'fin'")
+    print("- 4º Para finalizar la ejecucion del programa introduzca 'salir', para continuar con el programa introduzca la operacion que quiera realizar")
+
