b627652d2f799c8b0e64ae129a4d1ae45684445d
Joan Barres <joanbarressab@gmail.com>
2024-02-02 19:19:16 +0100

    Manejo de excepciones

--- main.py
+++ main.py
@@ -9,48 +9,56 @@ import operacions_extra.mitjana as mitjana
 import operacions_extra.primer as primer
 
 print("Iniciando calculadora...")
-
 menu.menu()
 
-operacion = input("Introduzca la operacion a realizar: ")
+while True:
+    operacion = input("Introduzca la operacion a realizar: ")
 
-def operador(caracter):
-    if caracter == "+":
-        numero1 = int(input("Introduzca el primer numero: "))
-        numero2 = int(input("Introduzca el segundo numero: "))
-        return suma.suma(numero1, numero2)
-    elif caracter == "-":
-        numero1 = int(input("Introduzca el primer numero: "))
-        numero2 = int(input("Introduzca el segundo numero: "))
-        return resta.resta(numero1, numero2)
-    elif caracter == "*":
-        numero1 = int(input("Introduzca el primer numero: "))
-        numero2 = int(input("Introduzca el segundo numero: "))
-        return multiplicacio.multiplicacio(numero1, numero2)
-    elif caracter == "/":
-        numero1 = int(input("Introduzca el primer numero: "))
-        numero2 = int(input("Introduzca el segundo numero: "))
-        return divisio.divisio(numero1, numero2)
-    elif caracter == "%":
-        numero1 = int(input("Introduzca el primer numero: "))
-        numero2 = int(input("Introduzca el segundo numero: "))
-        return divisio_entera.divisio_entera(numero1, numero2)
-    elif caracter == "!":
-        numero = int(input("Introduzca un numero: "))
-        return factorial.factorial(numero)
-    elif caracter == "?":
-        numero = int(input("Introduzca un numero: "))
-        return primer.primer(numero)
-    elif caracter == "@":
-        lista = []
-        while(True):
-            numero = input("Introduzca un numero: ")
-            if(numero != "fin"):
-                lista.append(int(numero))
-            else:
-                break
-        return mitjana.mitjana(lista)
+    if operacion != "salir":
+        try:
+            def operador(caracter):
+                if caracter == "+":
+                    numero1 = int(input("Introduzca el primer numero: "))
+                    numero2 = int(input("Introduzca el segundo numero: "))
+                    return suma.suma(numero1, numero2)
+                elif caracter == "-":
+                    numero1 = int(input("Introduzca el primer numero: "))
+                    numero2 = int(input("Introduzca el segundo numero: "))
+                    return resta.resta(numero1, numero2)
+                elif caracter == "*":
+                    numero1 = int(input("Introduzca el primer numero: "))
+                    numero2 = int(input("Introduzca el segundo numero: "))
+                    return multiplicacio.multiplicacio(numero1, numero2)
+                elif caracter == "/":
+                    numero1 = int(input("Introduzca el primer numero: "))
+                    numero2 = int(input("Introduzca el segundo numero: "))
+                    return divisio.divisio(numero1, numero2)
+                elif caracter == "%":
+                    numero1 = int(input("Introduzca el primer numero: "))
+                    numero2 = int(input("Introduzca el segundo numero: "))
+                    return divisio_entera.divisio_entera(numero1, numero2)
+                elif caracter == "!":
+                    numero = int(input("Introduzca un numero: "))
+                    return factorial.factorial(numero)
+                elif caracter == "?":
+                    numero = int(input("Introduzca un numero: "))
+                    return primer.primer(numero)
+                elif caracter == "@":
+                    lista = []
+                    while True:
+                        numero = input("Introduzca un numero: ")
+                        if numero != "fin":
+                            lista.append(int(numero))
+                        else:
+                            break
+                    return mitjana.mitjana(lista)
+                else:
+                    raise ValueError("Operación no válida")
+        except ValueError as valErr:
+            print(f"Error: {valErr}")
+        except ZeroDivisionError:
+            print("Error: No se puede dividir por cero")
+        except Exception as e:
+            print(f"Error inesperado: {e}")
     else:
-        print("Introduce una operacion valida")
-
-print(operador(operacion))
+        break
